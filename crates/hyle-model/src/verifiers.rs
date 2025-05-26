49661f882eb55ece74be269b8271a58249d77f7e
Alexandre Careil <alex.careil.fr@gmail.com>
2025-05-26 11:56:18 +0200

    Fix secp contract name

--- crates/hyle-model/src/verifiers.rs
+++ crates/hyle-model/src/verifiers.rs
@@ -13,7 +13,7 @@ pub enum NativeVerifiers {
     Secp256k1,
 }
 
-pub const NATIVE_VERIFIERS_CONTRACT_LIST: &[&str] = &["blst", "sha3_256", "secp156k1"];
+pub const NATIVE_VERIFIERS_CONTRACT_LIST: &[&str] = &["blst", "sha3_256", "secp256k1"];
 
 impl From<NativeVerifiers> for ProgramId {
     fn from(value: NativeVerifiers) -> Self {
