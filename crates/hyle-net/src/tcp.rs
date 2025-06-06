ef7e03e06320810a7780249d7c2f8048804c8051
Alexandre Careil <alex.careil.fr@gmail.com>
2025-06-06 11:49:53 +0200

    Remove from tcp

--- crates/hyle-net/src/tcp.rs
+++ crates/hyle-net/src/tcp.rs
@@ -10,29 +10,6 @@ use sdk::hyle_model_utils::TimestampMs;
 use tokio::task::JoinHandle;
 
 use anyhow::Result;
-use std::future::Future;
-
-use tokio::task::{self, JoinHandle};
-use tracing::error;
-
-/// Takes a task that is awaited in another task and logs a panic if it occurs.
-pub fn logged_task<F>(task: F) -> JoinHandle<F::Output>
-where
-    F: Future + Send + 'static,
-    F::Output: Send + 'static,
-{
-    task::spawn(async move {
-        let result = task::spawn(task).await;
-
-        match result {
-            Ok(res) => res,
-            Err(e) => {
-                error!("Task failed to complete: {:?}", e);
-                panic!("Task failed to complete: {:?}", e);
-            }
-        }
-    })
-}
 
 #[derive(Clone, BorshDeserialize, BorshSerialize, PartialEq)]
 pub enum TcpMessage {
