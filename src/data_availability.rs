b73c5fba9d0c02b4929a1a5d66345bb01cd5892d
Alexandre Careil <hhalex@users.noreply.github.com>
2025-06-24 16:36:45 +0200

    :bug: Data availability send if full (#1395)

--- src/data_availability.rs
+++ src/data_availability.rs
@@ -350,7 +350,9 @@ impl DataAvailability {
 
         // Send the block to NodeState for processing
         _ = log_error!(
-            self.bus.send(DataEvent::OrderedSignedBlock(block)),
+            self.bus
+                .send_waiting_if_full(DataEvent::OrderedSignedBlock(block))
+                .await,
             "Sending OrderedSignedBlock"
         );
 
@@ -504,7 +506,8 @@ pub mod tests {
             };
             _ = log_error!(
                 self.node_state_bus
-                    .send(NodeStateEvent::NewBlock(Box::new(full_block))),
+                    .send_waiting_if_full(NodeStateEvent::NewBlock(Box::new(full_block)))
+                    .await,
                 "Sending NodeState event"
             );
         }
