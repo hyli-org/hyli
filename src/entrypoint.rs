c170b0934b43197e49307d23ad718a22dd0521e1
Lancelot de Ferrière <wraitii@gmail.com>
2025-12-08 13:28:15 +0100

    fixup

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -80,7 +80,7 @@ impl RunPg {
             .await?;
 
         config.database_url = format!(
-            "postgres://postgres:postgres@localhost:{}/{{db}}",
+            "postgres://postgres:postgres@localhost:{}/postgres",
             pg.get_host_port_ipv4(5432).await?
         );
         config.run_indexer = true;
