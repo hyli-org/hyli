ba69e6c936009926a03598e8f55bb9f1d0de4b26
maximilien-hyle <maximilien@hyle.eu>
2026-01-28 17:50:16 +0100

    🐛 Add safety check in ModulesHandler::new()

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -205,7 +205,8 @@ async fn common_main(
     mut config: conf::Conf,
     crypto: Option<SharedBlstCrypto>,
 ) -> Result<ModulesHandler> {
-    std::fs::create_dir_all(&config.data_directory).context("creating data directory")?;
+    let bus = SharedMessageBus::new(BusMetrics::global(config.id.clone()));
+    let mut handler = ModulesHandler::new(&bus, config.data_directory.clone()).await?;
 
     // For convenience, when starting the node from scratch with an unspecified DB, we'll create a new one.
     // Handle this configuration rewrite before we print anything.
@@ -260,8 +261,6 @@ async fn common_main(
             }
         });
     }
-
-    let bus = SharedMessageBus::new(BusMetrics::global(config.id.clone()));
 
     let build_api_ctx = Arc::new(BuildApiContextInner {
         router: Mutex::new(Some(Router::new())),
@@ -325,8 +324,6 @@ async fn common_main(
             );
         }
     }
-
-    let mut handler = ModulesHandler::new(&bus, config.data_directory.clone()).await;
 
     if config.run_indexer {
         handler
